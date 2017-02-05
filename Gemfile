60629afc4550dcc27d2dbac92aa67b49c545eaed
lacfo <liyuanlacfo1990@gmail.com>
2017-02-05 11:13:38 +0800

    Add image column to product and implement uploader for image

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem "bootstrap-sass"
 gem "devise"
 gem "font-awesome-rails"
 gem "simple_form"
+gem "mini_magick"
+gem "carrierwave"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205024702) do
+ActiveRecord::Schema.define(version: 20170205030706) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170205024702) do
     t.text     "description"
     t.integer  "storage"
     t.decimal  "price"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
