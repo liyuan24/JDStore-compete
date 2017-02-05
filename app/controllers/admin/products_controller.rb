2291070c7d9edb1d407dff808081c3e2e3be8a10
lacfo <liyuanlacfo1990@gmail.com>
2017-02-05 11:49:17 +0800

    Implement admin:products#index and show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,4 +6,23 @@ class Admin::ProductsController < ApplicationController
 	def new 
 		@product = Product.new
 	end
+
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+			redirect_to admin_products_path, notice: "Create a new product"
+		else
+			render :new
+		end
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:title, :description, :storage, :price, :image)
+	end
 end
