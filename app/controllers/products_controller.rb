4a92f6c6787f9337f566ac30f87caa67f254a1f3
lacfo <liyuanlacfo1990@gmail.com>
2017-02-05 19:08:36 +0800

    Add search bar to navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+
+	before_action :validate_query_string, only: [:search]
+
 	def index
 		@products = Product.all
 	end
@@ -6,4 +9,14 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
 	end
+
+	def search
+		@products = Product.ransack({:title_cont => @q}).result(:distinct => true)
+	end
+
+	protected
+
+	def validate_query_string
+		@q = params[:query_string].gsub(/\|\'|\/|\?/, "") if params[:query_string].present?
+	end	
 end
