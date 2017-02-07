f800b7fc56be77cd9f98c1eafb14886f92199cd4
lacfo <liyuanlacfo1990@gmail.com>
2017-02-07 22:01:13 +0800

    User can buy when no storage and quantity can't exceed storage

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,8 +9,13 @@ class CartItemsController < ApplicationController
 	def update
 		@cart = current_cart
 		@cart_item = @cart.cart_items.find_by(product_id: params[:id])
-		@cart_item.update(cart_item_params)
-		redirect_to carts_path
+		if @cart_item.product.storage >= cart_item_params[:quantity].to_i
+			@cart_item.update(cart_item_params)
+			redirect_to carts_path
+		else
+			redirect_to carts_path, alert: "超出库存总量，请减少购买数量"
+		end
+		
 	end
 
 	private
