08c1c29eab9777e54aa117f4ea60f8d1131b6839
lacfo <liyuanlacfo1990@gmail.com>
2017-02-12 20:56:38 +0800

    Add slideimage to landing page

--- app/assets/javascripts/slideImage.js
+++ app/assets/javascripts/slideImage.js
@@ -1,25 +1,41 @@
-var slideIndex = 1;
+var slideIndex = 0;
 $(document).ready(function(){
 	$(window).load(function(){
-		showSlide(slideIndex);
+		showSlide();
 	});
 });
 
-function plusSlides(n){ showSlide(slideIndex += n); }
+// function plusSlides(n){ showSlide(slideIndex += n); }
 
-function currentSlides(n){ showSlide(slideIndex = n) }
+// function currentSlides(n){ showSlide(slideIndex = n) }
 
-function showSlide(n){
-	var i;
-	var slides = $(".mySlide");
-	var dots = $(".dot");
-	var len = slides.length;
-	if (n > len) { slideIndex = 1; }
-	if (n < 1) { slideIndex = len; }
-	for(i = 0; i < len; ++i){
-		slides[i].style.display = "none";
-		dots[i].className = dots[i].className.replace(" active-slide", "");
-	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active-slide";
+// function showSlide(n){
+// 	var i;
+// 	var slides = $(".mySlide");
+// 	var dots = $(".dot");
+// 	var len = slides.length;
+// 	if (n > len) { slideIndex = 1; }
+// 	if (n < 1) { slideIndex = len; }
+// 	for(i = 0; i < len; ++i){
+// 		slides[i].style.display = "none";
+// 		dots[i].className = dots[i].className.replace(" active-slide", "");
+// 	}
+// 	slides[slideIndex - 1].style.display = "block";
+// 	dots[slideIndex - 1].className += " active-slide";
+// }
+
+function showSlide() {
+    var i;
+    var slides = document.getElementsByClassName("mySlide");
+    var dots = document.getElementsByClassName("dot");
+    var len = slides.length;
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none"; 
+       dots[i].className = dots[i].className.replace(" active-slide", ""); 
+    }
+    slideIndex++;
+    if (slideIndex> len) {slideIndex = 1;}    
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active-slide";
+    setTimeout(showSlide, 2000); // Change image every 2 seconds
 }
