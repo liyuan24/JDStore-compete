7b49ffcf6a3b3c0f88f437652d46dbda05ddd8ac
lacfo <liyuanlacfo1990@gmail.com>
2017-02-13 01:51:57 +0800

    Move again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 // = require turbolinks
-//= require bootstrap-sprockets
 //= require jquery.elevatezoom
 //= require ratyrate
 //= require jquery.raty
