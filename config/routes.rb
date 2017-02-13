78f4970f8e5e9ce69e5bdf218a246561736f6f9e
lacfo <liyuanlacfo1990@gmail.com>
2017-02-13 15:01:22 +0800

    Nest reviews under products

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
   resources :products do
+    resources :reviews
   	collection do
   		get :search
   	end
