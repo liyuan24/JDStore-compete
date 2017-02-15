8afbb70d53d584017f6e0f7e44acce4309436dd1
lacfo <liyuanlacfo1990@gmail.com>
2017-02-15 09:59:36 +0800

    Create set_payment_method! and pay! method in order model and modify order#show

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,12 @@ class Order < ApplicationRecord
 		self.token = SecureRandom.uuid
 	end
 
+	def set_payment_with!(method)
+		self.update_column :payment_method, method
+	end
+
+	def pay!
+		self.update_column(is_paid: true)
+	end
+
 end
